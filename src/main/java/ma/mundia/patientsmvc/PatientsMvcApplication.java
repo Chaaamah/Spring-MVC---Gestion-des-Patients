1d6ddc31657f7218f11a6a2cf3d669c6a71d5061
Chaima Agoumi <chaimaeagoumi162@gmail.com>
2024-11-02 13:01:59 +0100

    Commit

---
+++ src/main/java/ma/mundia/patientsmvc/PatientsMvcApplication.java
@@ -0,0 +1,32 @@
+package ma.mundia.patientsmvc;
+
+import ma.mundia.patientsmvc.Entities.Patient;
+import ma.mundia.patientsmvc.Repository.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsMvcApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+        return  args -> {
+            /*patientRepository.save(new Patient(null, "Chama", new Date(), false, 12));
+            patientRepository.save(new Patient(null, "Jalila", new Date(), true, 400));
+            patientRepository.save(new Patient(null, "Bouchra", new Date(), false, 120));
+            patientRepository.save(new Patient(null, "Amine", new Date(), true, 30));*/
+
+            patientRepository.findAll().forEach( p -> {
+                System.out.println(p.getNom());
+            });
+        };
+    }
+}
---
+++ src/main/java/ma/mundia/patientsmvc/Repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package ma.mundia.patientsmvc.Repository;
+
+import ma.mundia.patientsmvc.Entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains(String kw, Pageable pageable);
+}
