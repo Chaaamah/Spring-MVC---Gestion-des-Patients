cbe402d9725ef8e56d54b75e84e9b5d0a64d65b3
Chaima Agoumi <chaimaeagoumi162@gmail.com>
2024-11-15 20:16:22 +0100

    Commit

--- src/main/java/ma/mundia/patientsmvc/Entities/Patient.java
+++ src/main/java/ma/mundia/patientsmvc/Entities/Patient.java
@@ -1,9 +1,13 @@
 package ma.mundia.patientsmvc.Entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -13,9 +17,13 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min = 4, max = 50)
     private String nom;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private boolean malade;
+    @DecimalMin("100")
     private int score;
 }
